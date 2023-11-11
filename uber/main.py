0ee807ad67dfa6f58207b82a29b1c73122d767ca
Andreas Chandra <phoenix.ichigo@yahoo.com>
2023-11-11 09:40:41 +0700

    add dataframe

--- uber/main.py
+++ uber/main.py
@@ -25,6 +25,8 @@ data_load_state = st.text("Loading data...")
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache_data)")
 
+st.dataframe(data)
+
 st.subheader("Number of pickups by hour")
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
 st.bar_chart(hist_values)
